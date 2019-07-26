429fe93855a357fc44ca6b8ffde2150401d86b85
Louis J <ljean@etud.insa-toulouse.fr>
2019-07-26 15:46:17 +0200

    Update README, remove obsolete script

--- get_libtorch.sh
+++
@@ -1,9 +0,0 @@
-LIBTORCH_LOCATION=$1
-echo "Copying libtorch from ${LIBTORCH_LOCATION}"
-
-mkdir libtorch
-rsync -arv ${LIBTORCH_LOCATION}/lib libtorch
-rsync -arv ${LIBTORCH_LOCATION}/bin libtorch
-rsync -arv ${LIBTORCH_LOCATION}/share libtorch
-rsync -arv ${LIBTORCH_LOCATION}/include libtorch
-
